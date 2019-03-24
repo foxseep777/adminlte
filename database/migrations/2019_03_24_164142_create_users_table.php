8771d34a46550cb63a525dd79044a8e69356e557
Charlie Root <foxsheep777@gmail.com>
2019-03-24 18:41:54 +0200

    first commit

---
+++ database/migrations/2019_03_24_164142_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->string('name', 256)->nullable();
+			$table->string('email', 256)->nullable();
+			$table->string('password', 256)->nullable();
+			$table->string('created_at', 20)->nullable();
+			$table->string('updated_at', 20)->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
